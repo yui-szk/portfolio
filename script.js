eb681dc23d915e16a6a374cb1a56cbcb020e6520
yui-szk <yuisuzuki.017@gmail.com>
2024-11-25 04:00:55 +0900

    Add scroll animation

---
+++ script.js
@@ -0,0 +1,19 @@
+// 対象が画面に入った時にアニメーションする
+let scrollAnimation = function () {
+  let scrollAnimationElm = document.querySelectorAll(".scroll-up");
+  let triggerMargin = 200;
+  scrollAnimationElm.forEach(function (elm) {
+    if (window.innerHeight > elm.getBoundingClientRect().top + triggerMargin) {
+      elm.classList.add("on");
+    }
+  });
+
+  // chart用アニメーション
+  let scrollChartAnimationElm = document.querySelectorAll(".skill__chart-bar");
+  scrollChartAnimationElm.forEach(function (elm) {
+    if (window.innerHeight > elm.getBoundingClientRect().top) {
+      elm.classList.add("skill__chart-animate");
+    }
+  });
+};
+window.addEventListener("scroll", scrollAnimation);
